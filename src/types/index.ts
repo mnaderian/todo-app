c5e39b0665c297d02c4d3ecf70d2c767a3b63a95
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-10-29 18:54:55 +0330

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// src/types/index.ts
+export interface Todo {
+  id: string
+  title: string
+  completed: boolean
+  userId: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface User {
+  id: string
+  email: string
+  name?: string
+}

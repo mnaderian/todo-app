5e7b8b20dfb323aa4bd82cf617995ec933d2b57e
Mahdi Naderian <naderian.mahdi@gmail.com>
2025-10-29 18:56:46 +0330

    wip

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-// src/lib/auth.ts
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@auth/prisma-adapter'
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
-// src/types/index.ts
 export interface Todo {
   id: string
   title: string
